a196dd8a17b33077caf19363caa4f1899b52cf23
annaturupi <annaturupi@gmail.com>
2018-04-30 12:00:33 +0200

    Lab7.2 remove duplicate pairs, parse heights

---
+++ README.md
@@ -0,0 +1,7 @@
+# Scrapy-lab
+
+Lab session 7: Using the Elastic Stack to study scraped data from a web page
+
+Students:
+* Lev Denisov
+* Anna Turu Pi
--- nytscraper/spiders/imdb.py
+++ nytscraper/spiders/imdb.py
@@ -14,10 +14,12 @@ class ImdbSpider(scrapy.Spider):
     name = 'imdb'
     allowed_domains = ['www.imdb.com']
     start_urls = ['https://www.imdb.com/title/tt0096463/fullcredits/']
+    movie_actor = {}
 
     def parse(self, response):
         movie_id = cleanString(response.css('meta[property="pageId"]::attr(content)').extract_first())
         movie_name = cleanString(response.css('h3[itemprop="name"] > a[itemprop="url"]::text').extract_first())
+        print(movie_name)
         noisy_year = cleanString(response.css('h3[itemprop="name"] > span.nobr::text').extract_first())
         movie_year = int(re.search('(\d+)',noisy_year).group(1))
         rows = response.css('table.cast_list tr')
@@ -27,6 +29,8 @@ class ImdbSpider(scrapy.Spider):
 
             actor_name = cleanString(r.css('td[itemprop="actor"] span::text').extract_first())
             actor_id = re.search('/name/(.+?)/',actor_url).group(1)
+            if self.check_movie_actor_pair(movie_id, actor_id): continue
+            self.add_movie_actor_pair(movie_id, actor_id)
             noisy_role = ''.join(r.css('td.character ::text').extract())
             noisy_character = cleanString(noisy_role)
             role_name = noisy_character.strip()
@@ -40,33 +44,55 @@ class ImdbSpider(scrapy.Spider):
             }
             request = response.follow('https://www.imdb.com/name/'+actor_id+'/bio', callback=self.parse_actor_bio)
             request.meta['item'] = json
-            request2 = response.follow('https://www.imdb.com/name/' + actor_id, callback=self.parse_actor_movies)
             yield request
+            request2 = response.follow('https://www.imdb.com/name/' + actor_id, callback=self.parse_actor_movies)
+            request2.meta['actor'] = actor_id
             yield request2
 
     def parse_actor_movies(self, response):
-        rows = response.css('div#filmo-head-actor+div.filmo-category-section>div')
+        actor_id = response.meta['actor']
+        rows = response.css('#filmo-head-actor+div.filmo-category-section>div, #filmo-head-actress+div.filmo-category-section>div')
         for r in rows:
+            if 'TV Series' in ''.join(r.css('::text').extract()): continue
             year_string = r.css('span.year_column::text').extract_first().strip()
             if not year_string: continue
             moviedate = int(year_string[:4])
             if moviedate > 1989 or moviedate < 1980: continue
             movie_id = re.search('(?:actor|actress)-(.+)', r.css('::attr(id)').extract_first()).group(1)
             movie_url = 'https://www.imdb.com/title/' + movie_id + '/fullcredits/'
+            if self.check_movie_actor_pair(movie_id, actor_id) :continue
             yield response.follow(movie_url, callback=self.parse)
 
     def parse_actor_bio(self, response):
         item = response.meta['item']
         if not response.css('#overviewTable').extract_first(): return item
-        birthdate = cleanString(response.css('#overviewTable tr:nth_child(1) time::attr(datetime)').extract_first())
+        tr = response.css('#overviewTable tr')
+        birthdate = ''
         birthname = ''
-        birthname_label = cleanString(response.css('#overviewTable tr:nth_child(2) td:nth_child(1)::text').extract_first())
-        if birthname_label == 'Birth Name':
-            birthname = cleanString(response.css('#overviewTable tr:nth_child(2) td:nth_child(2)::text').extract_first())
-        height = cleanString(response.css('#overviewTable tr:nth_child(3) td:nth_child(2)::text').extract_first())
+        height = ''
+        for r in tr:
+            label = r.css('td.label::text').extract_first()
+            if label == 'Born': birthdate = cleanString(r.css('time::attr(datetime)').extract_first()); continue
+            if label == 'Birth Name': birthname = cleanString(r.css('td:nth_child(2)::text').extract_first()); continue
+            if label == 'Height': height = self.get_height(r); continue
         item.update({
             'birthdate': birthdate,
             'birthname': birthname,
             'height': height
         })
         return item
+
+    def get_height(self, r):
+        noisy_height = cleanString(r.css('td:nth_child(2)::text').extract_first())
+        height = float(re.search('(\d+\.\d+)\sm', noisy_height).group(1))
+        return height
+
+    def create_movie_actor_key(self, m_id, a_id):
+        return m_id + '_' + a_id
+
+    def add_movie_actor_pair(self, movie_id, actor_id):
+        self.movie_actor[self.create_movie_actor_key(movie_id,actor_id)] = 1
+
+    def check_movie_actor_pair(self, movie_id, actor_id):
+        if self.create_movie_actor_key(movie_id, actor_id) in self.movie_actor: print('PAIR REPEATED: ' + movie_id + ' ' + actor_id); return True
+        return False
